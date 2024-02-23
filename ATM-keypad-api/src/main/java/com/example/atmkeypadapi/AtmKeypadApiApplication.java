2169c4bd4bed1763b7fa9ed5cbb4613104a2481e
Bruno Ceresér <bruno.cereser@catolicasc.edu.br>
2024-02-23 20:25:24 -0300

    Added API base

---
+++ ATM-keypad-api/src/main/java/com/example/atmkeypadapi/AtmKeypadApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.atmkeypadapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmKeypadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmKeypadApiApplication.class, args);
+	}
+
+}
