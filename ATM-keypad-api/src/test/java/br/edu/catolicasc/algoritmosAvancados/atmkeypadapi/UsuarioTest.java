30d38e68e83e488a92086b674efa4e3dc32d3468
Vandelson Cleitoso <julim.generic@gmail.com>
2024-03-01 19:56:58 -0300

    Métodos e testes de usuário

---
+++ ATM-keypad-api/src/test/java/br/edu/catolicasc/algoritmosAvancados/atmkeypadapi/UsuarioTest.java
@@ -0,0 +1,29 @@
+package br.edu.catolicasc.algoritmosAvancados.atmkeypadapi;
+
+
+import org.junit.jupiter.api.Test;
+
+import br.edu.catolicasc.algoritmosAvancados.Usuario;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UsuarioTest {
+    @Test
+    public void testCriptografiaAES() {
+        String nome = "testUser";
+        String password = "testPassword";
+        Usuario usuario = new Usuario();
+        usuario.setNome(nome);
+        usuario.setPassword(password);
+
+        // Test encryption and decryption
+        String encryptedPassword = Usuario.CriptografiaAES.criptografar(password, nome);
+        String decryptedPassword = Usuario.CriptografiaAES.decriptografar(encryptedPassword, nome);
+
+        assertEquals(password, decryptedPassword, "Decrypted password should match the original password");
+
+        // Test password setter and getter
+        assertEquals(usuario.getPassword(), decryptedPassword, "Password returned by getter should match the original password");
+    }
+    
+}
