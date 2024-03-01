74be74fe3f28c94ee1f83b5981d61f2ff0233af7
FernandoLuiis <fpaludo2018@gmail.com>
2024-03-01 20:11:23 -0300

    Front inclusão de buttons

--- senha_caixa_banco/src/App.js
+++ senha_caixa_banco/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Button from './components/Button';
 
 function App() {
+  const handleButtonClick = (label) => {
+    // Lógica para tratar o clique nos botões
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="row">
+        <Button label="3 ou 9" onClick={() => handleButtonClick("3 ou 9")} />
+        <Button label="0 ou 5" onClick={() => handleButtonClick("0 ou 5")} />
+        <Button label="2 ou 8" onClick={() => handleButtonClick("2 ou 8")} />
+      </div>
+      <div className="row">
+        <Button label="1 ou 4" onClick={() => handleButtonClick("1 ou 4")} />
+        <Button label="6 ou 7" onClick={() => handleButtonClick("6 ou 7")} />
+        <Button label="Apagar" onClick={() => handleButtonClick("Apagar")} />
+      </div>
+      <div className="row">
+        <Button label="Acessar" onClick={() => handleButtonClick("Acessar")} fullWidth />
+      </div>
     </div>
   );
 }
---
+++ senha_caixa_banco/src/components/Button.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Button({ label, onClick, clicado, fullWidth }) {
+  const estiloBotao = {
+    fontSize: '32px',
+    width: fullWidth ? '100%' : '200px',
+    height: '80px',
+    margin: '10px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    cursor: 'pointer',
+    borderRadius: '12px',
+    boxShadow: '0px 2px 6px rgba(0, 0, 0, 0.2)',
+    transition: 'box-shadow 0.3s ease',
+  };
+
+  const estiloRotulo = {
+    visibility: clicado ? 'hidden' : 'visible',
+  };
+
+  const handleClick = () => {
+    onClick();
+    const audio = new Audio('/button-click-sound.mp3'); // Adicione um arquivo de som chamado 'button-click-sound.mp3' na pasta public/
+    audio.play();
+  };
+
+  return (
+    <button onClick={handleClick} style={estiloBotao}>
+      <span style={estiloRotulo}>{label}</span>
+    </button>
+  );
+}
+
+export default Button;
