70bd61acb1d38853ec4d58f70a01eab7cd10731b
FernandoLuiis <fpaludo2018@gmail.com>
2024-03-04 22:24:58 -0300

    Front end posicionamento e função de algumas teclas

--- senha_caixa_banco/src/App.js
+++ senha_caixa_banco/src/App.js
@@ -1,30 +1,43 @@
 import React, { useState } from 'react';
 import Button from './components/Button';
+import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   const [options, setOptions] = useState('');
 
   const handleButtonClick = (label) => {
-    setOptions(options + label);
+    if (label === "Apagar") {
+      setOptions('');
+    } else {
+      setOptions(options + label);
+    }
   };
 
+  const maskedOptions = options.replace(/./g, '*');
+
   return (
-    <div>
-      <div>
+    <div className="app-container">
+      <div className="button-row">
         <Button label="3 ou 9" onClick={() => handleButtonClick("3 ou 9")} />
         <Button label="0 ou 5" onClick={() => handleButtonClick("0 ou 5")} />
         <Button label="2 ou 8" onClick={() => handleButtonClick("2 ou 8")} />
       </div>
-      <div>
+      <div className="button-row">
         <Button label="1 ou 4" onClick={() => handleButtonClick("1 ou 4")} />
         <Button label="6 ou 7" onClick={() => handleButtonClick("6 ou 7")} />
-        <Button label="Apagar" onClick={() => handleButtonClick("Apagar")} />
+        <Button
+          onClick={() => handleButtonClick("Apagar")}
+          icon={<FontAwesomeIcon icon={faTrash} />}
+        />
       </div>
-      <div>
+      <div className="options-container">
+        <p className="options-description">Digite sua senha:</p>
+        <p className="options">{maskedOptions}</p>
+      </div>
+      <div className="button-row">
         <Button label="Acessar" onClick={() => handleButtonClick("Acessar")} fullWidth />
-      </div>
-      <div>
-        <p>Opções digitadas: {options}</p>
       </div>
     </div>
   );
